55b930b3fa59a0d48c32f7a360f8f772b10e9e07
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2025-02-23 17:17:29 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,13 +1,21 @@
 'use strict';
 
-// const express = require('express');
+import express from 'express';
+import usersRouter from './routes/users.route.js';
+import expensesRouter from './routes/expenses.route.js';
 
 function createServer() {
   // Use express to create a server
   // Add a routes to the server
   // Return the server (express app)
+
+  const app = express();
+
+  app.use('/users', express.json(), usersRouter);
+
+  app.use('/expenses', express.json(), expensesRouter);
+
+  return app;
 }
 
-module.exports = {
-  createServer,
-};
+export { createServer };
